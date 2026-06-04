00939aa2038e12986c1bae0f64f9df30722b83f7
1111 <a1111@MacBook-Air-1111.local>
2026-06-04 17:18:30 +0300

    fix pooler statement cache

--- database/db.py
+++ database/db.py
@@ -9,7 +9,7 @@ _pool = None
 async def get_pool():
     global _pool
     if _pool is None:
-        _pool = await asyncpg.create_pool(DATABASE_URL, min_size=1, max_size=10)
+        _pool = await asyncpg.create_pool(DATABASE_URL, min_size=1, max_size=10, statement_cache_size=0)
     return _pool
 
 
